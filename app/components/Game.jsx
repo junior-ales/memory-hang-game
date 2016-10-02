4d7c6bd292da68a07ef2536c9eb6289de95a72b6
Junior Ales <edilson.ales.jr@gmail.com>
2016-10-02 02:25:44 +0100

    ramdomly getting a word

--- app/components/Game.jsx
+++ app/components/Game.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { shuffle, assign, toArray, difference, take, drop, zipObject, range, flatten, sampleSize } from 'lodash';
+import { groupBy, uniq, shuffle, assign, toArray, difference, take, drop, zipObject, range, flatten, sampleSize, sample } from 'lodash';
 
 import Cell from './Cell';
 import Row from './Row';
 import Footer from './Footer';
 
 const ALPHABET = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+const WORDS = groupBy(uniq(['as', 'raizes', 'etimologicas', 'do', 'termo', 'brasil', 'sao', 'de', 'dificil', 'reconstrucao', 'o', 'filologo', 'adelino', 'jose', 'da', 'silva', 'azevedo', 'postulou', 'que', 'se', 'trata', 'de', 'uma', 'palavra', 'de', 'procedencia', 'celta', 'uma', 'lenda', 'que', 'fala', 'de', 'uma', 'terra', 'de', 'delicias', 'vista', 'entre', 'nuvens', 'mas', 'advertiu', 'tambem', 'que', 'as', 'origens', 'mais', 'remotas', 'do', 'termo', 'poderiam', 'ser', 'encontradas', 'na', 'lingua', 'dos', 'antigos', 'fenicios', 'na', 'epoca', 'colonial', 'cronistas', 'da', 'importancia', 'de', 'joao', 'de', 'barros', 'frei', 'vicente', 'do', 'salvador', 'e', 'pero', 'de', 'magalhaes', 'gandavo', 'apresentaram', 'explicacoes', 'concordantes', 'acerca', 'da', 'origem', 'do', 'nome', 'brasil', 'de', 'acordo', 'com', 'eles', 'o', 'nome', 'brasil', 'e', 'derivado', 'de', 'pau', 'brasil', 'designacao', 'dada', 'a', 'um', 'tipo', 'de', 'madeira', 'empregada', 'na', 'tinturaria', 'de', 'tecidos', 'na', 'epoca', 'dos', 'descobrimentos', 'era', 'comum', 'aos', 'exploradores', 'guardar', 'cuidadosamente', 'o', 'segredo', 'de', 'tudo', 'quanto', 'achavam', 'ou', 'conquistavam', 'a', 'fim', 'de', 'explora', 'lo', 'vantajosamente', 'mas', 'nao', 'tardou', 'em', 'se', 'espalhar', 'na', 'europa', 'que', 'haviam', 'descoberto', 'certa', 'ilha', 'brasil', 'no', 'meio', 'do', 'oceano', 'atlantico', 'de', 'onde', 'extraiam', 'o', 'pau', 'brasil', 'madeira', 'cor', 'de', 'brasa', 'antes', 'de', 'ficar', 'com', 'a', 'designacao', 'atual', 'brasil', 'as', 'novas', 'terras', 'descobertas', 'foram', 'designadas', 'de', 'monte', 'pascoal', 'quando', 'os', 'portugueses', 'avistaram', 'terras', 'pela', 'primeira', 'vez', 'ilha', 'de', 'vera', 'cruz', 'terra', 'de', 'santa', 'cruz', 'nova', 'lusitania', 'cabralia', 'imperio', 'do', 'brasil', 'e', 'estados', 'unidos', 'do', 'brasil', 'os', 'habitantes', 'naturais', 'do', 'brasil', 'sao', 'denominados', 'brasileiros', 'cujo', 'gentilico', 'e', 'registrado', 'em', 'portugues', 'a', 'partir', 'de', '', 'que', 'se', 'referia', 'inicialmente', 'apenas', 'aos', 'que', 'comercializavam', 'pau', 'brasil']), 'length');
 
 class Game extends React.Component {
   constructor(props) {
@@ -17,7 +19,7 @@ class Game extends React.Component {
       return range(columns).map(colId => `${rowId}${colId}`);
     });
 
-    this.word = 'junior';
+    this.word = sample(WORDS[this.props.gameLevel.wordSize]);
     const letterCells = sampleSize(flatten(this.matrix), this.props.gameLevel.wordSize + this.props.gameLevel.wrongLettersQuantity);
     const wrongLetterCells = drop(letterCells, this.props.gameLevel.wordSize);
     this.correctLetterCells = take(letterCells, this.props.gameLevel.wordSize);
